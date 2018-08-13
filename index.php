ee3c05578554354765ff922e069ee07f2f1fb6a2
thierry halot <thierry.halot@laposte.net>
2018-08-13 17:44:08 +0200

    include du template

--- index.php
+++ index.php
@@ -4,5 +4,11 @@ use BWB\Framework\mvc\Routing;
 // pour beneficier de l'autoload de composer
 include "vendor/autoload.php";
 
+include "views/template/top.php";
+
+
 // A chaque requete emise nous lançons le mecanisme de routage
 (new Routing())->execute();
+
+
+include "views/template/bottom.php";
