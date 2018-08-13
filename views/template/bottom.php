28a3d09d8dcbfd973947543b674350fc30bf2299
thierry halot <thierry.halot@laposte.net>
2018-08-13 17:42:48 +0200

    import du css et js

---
+++ views/template/bottom.php
@@ -0,0 +1,7 @@
+</body>
+<script src="public\js\jquery.js"></script>
+<script src="public/bootstrap-4.1.3/js/bootstrap.bundle.js"></script>
+<script src="public/bootstrap-4.1.3/js/bootstrap.js"></script>
+
+
+</html>
---
+++ views/template/top.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="public/bootstrap-4.1.3/css/bootstrap.css">
+    <link rel="stylesheet" href="public\bootstrap-4.1.3\css\bootstrap-grid.css">
+    <link rel="stylesheet" href="public/bootstrap-4.1.3/css/bootstrap-reboot.css">
+</head>
+<body>
