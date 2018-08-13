0ad633274a7ac2f7e7b3234b4a9ef79e497ea972
thierry halot <thierry.halot@laposte.net>
2018-08-13 17:45:14 +0200

    creation du fichier de vue pour la partie front

---
+++ views/front.php
@@ -0,0 +1,13 @@
+<?= $user->getNom();?>
+<h1> Mon front </h1>
+<div class="dropdown">
+    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+        Dropdown button
+    </button>
+    <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Link 1</a>
+        <a class="dropdown-item" href="#">Link 2</a>
+        <a class="dropdown-item" href="#">Link 3</a>
+    </div>
+
+</div>
