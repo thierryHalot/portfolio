37d7cef05d46afcccbd11b3982037c5f12e5a444
thierry halot <thierry.halot@laposte.net>
2018-08-13 17:51:01 +0200

    creation d'un DAOCommentaire

---
+++ dao/DAOCommentaire.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fatcobra
+ * Date: 13/08/2018
+ * Time: 11:03
+ */
+
+namespace BWB\Framework\mvc\dao;
+
+
+use BWB\Framework\mvc\DAO;
+
+class DAOCommentaire extends DAO
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function create($array)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function retrieve($id)
+    {
+        // TODO: Implement retrieve() method.
+    }
+
+    public function update($array)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function getAll()
+    {
+        // TODO: Implement getAll() method.
+    }
+
+    public function getAllBy($filter)
+    {
+        // TODO: Implement getAllBy() method.
+    }
+
+
+}
