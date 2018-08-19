7f36f2ceeeb664494adc5ed0bdc43efc38b1ed30
thierry halot <thierry.halot@laposte.net>
2018-08-19 12:10:05 +0200

    modif vue front

--- views/front.php
+++ views/front.php
@@ -1,13 +1,237 @@
-<?= $user->getNom();?>
-<h1> Mon front </h1>
-<div class="dropdown">
-    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
-        Dropdown button
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Accueil</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="dropdown-menu">
-        <a class="dropdown-item" href="#">Link 1</a>
-        <a class="dropdown-item" href="#">Link 2</a>
-        <a class="dropdown-item" href="#">Link 3</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Présentation <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Compétence</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Projets</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link " href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!-- Accueil -->
+
+<div class="container">
+    <div class="block col-xs-4">
+        <div class="card"><img class="card-img img-responsive" src="public/img/photo.jpg" alt="photoAccueil" />
+                <h1 class="card-title text-center">Thierry Halot</h1>
+            <h1 class="card-title text-center">Développeur logiciel</h1>
+                <p class="card-text text-center">(En recherche d'un stage)</p>
+        </div>
     </div>
 
+
+
 </div>
+<br>
+
+<!-- Presentation -->
+<div class="container">
+    <h1><i class="fas fa-grin"></i></i>Qui suis-je ?</h1>
+    <br>
+<div>
+    <p>« Texte » est issu du mot latin « textum », dérivé du verbe « texere » qui signifie « tisser ». Le mot s'applique à l'entrelacement des fibres utilisées dans le tissage, voir par exemple Ovide : « Quo super iniecit textum rude sedula Baucis = (un siège) sur lequel Baucis empressée avait jeté un tissu grossier »2 ou au tressage (exemple chez Martial « Vimineum textum = panier d'osier tressé »). Le verbe a aussi le sens large de construire comme dans « basilicam texere = construire une basilique » chez Cicéron3.
+
+        Le sens figuré d'éléments de langage organisés et enchaînés apparaît avant l'Empire romain : il désigne un agencement particulier du discours. Exemple : « epistolas texere = composer des épîtres » - Cicéron (Ier siècle av. J.-C.)4 ou plus nettement chez Quintilien (Ier siècle apr. J.-C.) : « verba in textu jungantur = l'agencement des mots dans la phrase »5.
+
+        Les formes anciennes du Moyen Âge désignent au XIIe siècle le volume qui contient le texte sacré des Évangiles, puis au XIIIe siècle. le texte original d'un livre saint ou des propos de quelqu'un. Au XVIIe siècle le mot s’applique au passage d'un ouvrage pris comme référence et au début du XIXe siècle le mot texte a son sens général d'« écrit »6. </p>
+</div>
+    <div class="row">
+        <div class="col-md-3 col-xs-6">
+            <h1 class="text-center"><i class="fas fa-arrow-alt-circle-up"></i></h1>
+            <p>je m'appelle m'achin bidul chouette et j'aime les pattes,c'est coul bordel de test de texte super long,
+                on va voir ce que sa donne ,j'uttilise pas le lorum itsim mais pourquoi donc,les chaussette de l'archiduchesse sont t'elle sèche,
+                oui archi séche</p>
+        </div>
+        <div class="col-md-3 col-xs-6">
+            <h1 class="text-center"><i class="fas fa-arrow-alt-circle-up"></i></h1>
+            <p>je m'appelle m'achin bidul chouette et j'aime les pattes,c'est coul bordel de test de texte super long,
+                on va voir ce que sa donne ,j'uttilise pas le lorum itsim mais pourquoi donc,les chaussette de l'archiduchesse sont t'elle sèche,
+                oui archi séche</p>
+        </div>
+        <div class="col-md-3 col-xs-6">
+            <h1 class="text-center"><i class="fas fa-arrow-alt-circle-up"></i></h1>
+            <p>je m'appelle m'achin bidul chouette et j'aime les pattes,c'est coul bordel de test de texte super long,
+                on va voir ce que sa donne ,j'uttilise pas le lorum itsim mais pourquoi donc,les chaussette de l'archiduchesse sont t'elle sèche,
+                oui archi séche</p>
+        </div>
+        <div class="col-md-3 col-xs-6">
+            <h1 class="text-center"><i class="fas fa-arrow-alt-circle-up"></i></h1>
+            <p>je m'appelle m'achin bidul chouette et j'aime les pattes,c'est coul bordel de test de texte super long,
+                on va voir ce que sa donne ,j'uttilise pas le lorum itsim mais pourquoi donc,les chaussette de l'archiduchesse sont t'elle sèche,
+                oui archi séche</p>
+        </div>
+    </div>
+<br>
+    <form method="get" action="public/document/CV_Thierry_Halot.pdf">
+    <input type="submit" value="Télécharger mon cv" class="btn btn-primary float-lg-right" onclick="window.location='<?= "http://".$_SERVER['HTTP_HOST']."/public/document/CV_Thierry_Halot.pdf"  ?>';">
+        <br>
+    </form>
+</div>
+<br>
+<!-- Competence -->
+<div class="container">
+    <br>
+    <h1><i class="fas fa-arrow-alt-circle-up"></i>Mes Compétences</h1>
+<div>
+    <p>HTML</p>
+    <div class="fondBarProgresse" >
+        <div class="skills html" id="skillHtml">90%</div>
+    </div>
+
+    <p>CSS</p>
+    <div class="fondBarProgresse">
+        <div class="skills css" id="skillCss">80%</div>
+    </div>
+
+    <p>JavaScript</p>
+    <div class="fondBarProgresse">
+        <div class="skills js" id="skillJs">65%</div>
+    </div>
+    <p>Ajax</p>
+    <div class="fondBarProgresse">
+        <div class="skills ajax" id="skillAjax">65%</div>
+    </div>
+    <p>PHP</p>
+    <div class="fondBarProgresse">
+        <div class="skills php" id="skillPhp">60%</div>
+    </div>
+    <p>Symfony</p>
+    <div class="fondBarProgresse">
+        <div class="skills symfony" id="skillSymfony">60%</div>
+    </div>
+    <p>SQL</p>
+    <div class="fondBarProgresse">
+        <div class="skills sql" id="skillSql">60%</div>
+    </div>
+</div>
+</div>
+<br>
+<!-- Projets -->
+<div class="container">
+    <h1><i class="fas fa-book-reader"></i>Mes Projets</h1>
+    <br>
+    <div class="row">
+        <div class="col-md-4">
+
+            <div class="card">
+                <h1 class="card-title text-center">Nom projet</h1>
+                <img class="card-img img-responsive" src="public/img/photo.jpg" alt="photoAccueil" />
+
+            </div>
+        </div>
+        <div class="col-md-4">
+
+            <div class="card">
+                <h1 class="card-title text-center">Nom projet</h1>
+                <img class="card-img img-responsive" src="public/img/photo.jpg" alt="photoAccueil" />
+
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <h1 class="card-title text-center">Nom projet</h1>
+                <img class="card-img img-responsive" src="public/img/photo.jpg" alt="photoAccueil" />
+
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <h1 class="card-title text-center">Nom projet</h1>
+                <img class="card-img img-responsive" src="public/img/photo.jpg" alt="photoAccueil" />
+
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <h1 class="card-title text-center">Nom projet</h1>
+                <img class="card-img img-responsive" src="public/img/photo.jpg" alt="photoAccueil" />
+
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <h1 class="card-title text-center">Nom projet</h1>
+                <img class="card-img img-responsive" src="public/img/photo.jpg" alt="photoAccueil" />
+
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<!-- Contact -->
+<div class="container">
+    <h1><i class="far fa-id-card"></i>Contact</h1>
+    <br>
+    <div class="media">
+        <img class="mr-3" src="public/img/petit.jpg" alt="Generic placeholder image">
+        <div class="media-body">
+           <p>
+               Nom:<br>
+               Prenom:<br>
+               Tel:<br>
+               Adresse:<br>
+               Code postal:<br>
+               Ville:<br>
+           </p>
+        </div>
+    </div>
+    <br>
+    <form>
+<h6>Me contacter :</h6>
+        <br>
+        <div class="form-group row" >
+            <label for="nomContact" class="col-sm-2 col-form-label">Nom</label>
+            <div class="col-sm-10">
+            <input type="text" class="form-control" id="nomContact" placeholder="Example input">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="prenomContact" class="col-sm-2 col-form-label">Prenom</label>
+            <div class="col-sm-10">
+            <input type="text" class="form-control" id="prenomContact" placeholder="Another input">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-10">
+                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="messageContact">Message:</label>
+            <textarea class="form-control" rows="5" id="messageContact" name="text"></textarea>
+        </div>
+        <div>
+        <button type="submit" class="btn btn-primary float-right">Submit</button>
+            <br>
+        </div>
+    </form>
+</div>
+<footer>
+<div class="container">
+    <div class="text-center center-block">
+        <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+        <a href="https://twitter.com/bootsnipp"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+        <a href="https://plus.google.com/+Bootsnipp-page"><i id="social-gp" class="fa fa-linkedin-square fa-3x social"></i></a>
+        <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-github fa-3x social"></i></a>
+    </div>
+    <hr>
+</div>
+</footer>
--- views/template/bottom.php
+++ views/template/bottom.php
@@ -1,7 +1,11 @@
+
+
 </body>
-<script src="public\js\jquery.js"></script>
-<script src="public/bootstrap-4.1.3/js/bootstrap.bundle.js"></script>
-<script src="public/bootstrap-4.1.3/js/bootstrap.js"></script>
 
-
+<script type="text/javascript" src="/public\js\jquery.js" ></script>
+<script type="text/javascript" src="/public/bootstrap-4.1.3/js/bootstrap.bundle.js" ></script>
+<script type="text/javascript" src="/public/bootstrap-4.1.3/js/bootstrap.js" ></script>
+<script type="text/javascript" src="/public/bootstrap-4.1.3/js/bootstrap.js" ></script>
+<script type="text/javascript" src="/public/js/app.js" ></script>
+<script type="text/javascript" src="/public/js/barProgresse.js" ></script>
 </html>
