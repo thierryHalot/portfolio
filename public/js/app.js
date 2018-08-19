3fea99f551d04f25e9eae72efee0b08a9715253a
thierry halot <thierry.halot@laposte.net>
2018-08-19 14:57:19 +0200

    fonction pour affecter une hauteur a mes elemnet

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,31 @@
-getUsers();
-var xhr = new XMLHttpRequest();
-alert(xhr.open('GET', 'thierry-halot.fr/api/user'));
+// getUsers();
+// var xhr = new XMLHttpRequest();
+// alert(xhr.open('GET', 'thierry-halot.fr/api/user'));
+//
+//
+// function getUsers() {
+//     $.ajax({
+//         type: "GET",
+//         url: "thierry-halot.fr/api/user/",
+//         data: data,
+//
+//         success: function (data) {
+//             alert(data);
+//             alert("oui");
+//         },
+//
+//         error:function(data) {
+//                 console.log(data);
+//             alert("non");
+//             }
+//
+//     })
+// }
 
-
-function getUsers() {
-    $.ajax({
-        type: "GET",
-        url: "thierry-halot.fr/api/user/",
-        data: data,
-
-        success: function (data) {
-            alert(data);
-            alert("oui");
-        },
-
-        error:function(data) {
-                console.log(data);
-            alert("non");
-            }
-
-    })
-}
+$(function() {
+    // Sections height
+    $(window).resize(function() {
+        var sH = $(window).height();
+        $('.contenue').css('height', sH + 'px');
+    });
+});
