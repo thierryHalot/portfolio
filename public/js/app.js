eeeb4657e434e1f7d6dc4849ee94d278a0321116
Thierry <thierry.halot@laposte.net>
2018-08-21 09:08:56 +0200

    requete ajax fonctionnel

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,26 @@
-// getUsers();
+alert("coucou");
+getUsers();
 // var xhr = new XMLHttpRequest();
 // alert(xhr.open('GET', 'thierry-halot.fr/api/user'));
 //
 //
-// function getUsers() {
-//     $.ajax({
-//         type: "GET",
-//         url: "thierry-halot.fr/api/user/",
-//         data: data,
-//
-//         success: function (data) {
-//             alert(data);
-//             alert("oui");
-//         },
-//
-//         error:function(data) {
-//                 console.log(data);
-//             alert("non");
-//             }
-//
-//     })
-// }
+
+ function getUsers() {
+     $.ajax({
+         type: "GET",
+         url: "http://thierry-halot.bwb/api/user",
+
+        dataType: 'json',
+         success: function (data) {
+             console.log(data);
+             },
+
+         error:function(data) {
+             alert("non");
+             }
+
+     })
+ }
 
 $(function() {
     // Sections height
