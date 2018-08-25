9cde5830cb8c92c798b02be1dedde0d48e05a5d2
Thierry <thierry.halot@laposte.net>
2018-08-25 23:23:45 +0200

    fonction getAllby operationnel

--- dao/DAOCompte_reseaux.php
+++ dao/DAOCompte_reseaux.php
@@ -78,9 +78,48 @@ class DAOCompte_reseaux extends DAO
         return $entities;
     }
 
+    //fonction qui permet de retrouver un compte reseaux par rapport a une colonne
+    //prend en argument un tableau associatif corespondant a l'element que je souhaite rechercher :
+    // la clé coressespondant à la colonne que l'on souhaite recherche
+    // en valeur : la valeur qui nous interresse
+    //retourne un tableau peuplé des entité correspond a ma recherche
     public function getAllBy($filter)
     {
-        // TODO: Implement getAllBy() method.
+        $sql = "SELECT * FROM compte_reseaux";
+        $i = 0;
+
+        //je boucle dans mon tableau
+        foreach($filter as $key => $value){
+
+//a la premiere iteration  je concataine ma variable $sql avec " WHERE " suivie de la clé /valeur de $filter"
+            if($i===0){
+                $sql .= " WHERE ";
+//a toute les autres itération je rajoute des "AND" suivi des clée/valeur dans le cas d'une recherche plus poussé
+            } else {
+                $sql .= " AND ";
+            }
+            $sql .= $key . " = '" . addslashes($value) . "'";
+
+            $i++;
+        };
+
+        $entities = array();
+        $statement = $this->getPdo()->query($sql);
+        $results = $statement->fetchAll();
+        foreach($results as $result){
+
+            $entity = new Compte_reseaux();
+            $entity->setLien($result['lien']);
+            $entity->setImg($result['img']);
+            $entity->setUser_iduser((int)$result['user_iduser']);
+            $entity->setNom($result['nom']);
+
+            $entity->setIdcompte_reseaux((int)$result['idcompte_reseaux']);
+
+            array_push($entities,$entity);
+        };
+
+        return $entities;
     }
 
 }
