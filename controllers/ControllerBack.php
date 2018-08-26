861ca0fdc4f48d8463a2c511564f37980c3a7b95
Thierry <thierry.halot@laposte.net>
2018-08-26 19:36:29 +0200

    injection template dans la vue

--- controllers/ControllerBack.php
+++ controllers/ControllerBack.php
@@ -10,13 +10,37 @@ namespace BWB\Framework\mvc\controllers;
 
 
 use BWB\Framework\mvc\Controller;
+use BWB\Framework\mvc\dao\DAOCompetence;
+use BWB\Framework\mvc\dao\DAOCompte_reseaux;
+use BWB\Framework\mvc\dao\DAOCoordonne;
+use BWB\Framework\mvc\dao\DAODiplomes;
+use BWB\Framework\mvc\dao\DaoExp_pro;
+use BWB\Framework\mvc\dao\DAOFonctionnalite;
+use BWB\Framework\mvc\dao\DAOProjets;
+use BWB\Framework\mvc\dao\DAOUser;
+use BWB\Framework\mvc\models\Competence;
+use BWB\Framework\mvc\models\Compte_reseaux;
+use BWB\Framework\mvc\models\Coordonne;
+use BWB\Framework\mvc\models\Diplome;
+use BWB\Framework\mvc\models\Experience_pro;
+use BWB\Framework\mvc\models\Fonctionnalite;
+use BWB\Framework\mvc\models\Projets;
+use BWB\Framework\mvc\models\User;
 
 class ControllerBack extends Controller
 {
 
     public function getView(){
 
+        $top = file_get_contents("views/template/top.php");
+        $bottom = file_get_contents("views/template/bottom.php");
 
-        $this->render("back");
+
+
+        $this->render("back", array(
+
+            "top"=>$top,
+            "bottom"=>$bottom
+        ));
     }
 }
