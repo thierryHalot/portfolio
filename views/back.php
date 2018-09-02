e401ce52b9d93de7e328c7b4d579895b4c780f3b
Thierry <thierry.halot@laposte.net>
2018-09-02 14:47:53 +0200

    interface + traitement crud formulaire exp pro fonctionnel

--- views/back.php
+++ views/back.php
@@ -1,6 +1,6 @@
 <?= $top ?>
 
-<!--<!-- formulaire Update User -->-->
+<!--<!-- formulaire Update User -->
 <!--    <div class="container contenue" >-->
 <!--        <br>-->
 <!--        <form id="updateUser" method="post" action="/updateUser">-->
@@ -118,7 +118,7 @@
 <!--        </form>-->
 <!--    </div>-->
 
-<!-- test table -->
+<!-- Crud diplome table -->
     <div class="container">
     <div class="table-wrapper">
         <div class="table-title">
@@ -315,6 +315,202 @@
                         <div class="form-group">
                             <label for="descriptionDiplomesCreate">description:</label>
                             <textarea class="form-control" rows="5" name="descriptionDiplomesCreate" id="descriptionDiplomesCreate" required ></textarea>
+                        </div>
+                    </div>
+
+                    <!-- Modal footer -->
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary float-right">Crée</button>
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Fermer</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- Crud Experience Pro table -->
+    <div class="container">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h2>Liste des Expériences profesionnels</h2>
+                    </div>
+                    <div class="col-sm-6">
+                        <button type="button" class="btn btn-primary fas fa-plus-circle float-right" data-toggle="modal" data-target="#createExpPro"> Exp Pro</button>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <table class="table table-responsive-sm table-hover table-striped">
+
+
+                <thead class="table-light">
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">date d'entrer</th>
+                    <th scope="col">date de sortie</th>
+                    <th scope="col">nom de boite</th>
+                    <th scope="col"></th>
+                    <th scope="col">edit</th>
+                    <th scope="col">detail</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($expPros as $expPro): ?>
+                <tr>
+                    <th scope="row"><?= $expPro->getIdexperience_pro() ?></th>
+                    <td><?= $expPro->getDate_entrer() ?></td>
+                    <td><?= $expPro->getDate_sortie() ?></td>
+                    <td><?= $expPro->getNom_boite() ?></td>
+                    <!--Affichage du bouton de suppression -->
+                    <td>
+
+                        <form method="post" action="/deleteExpPro">
+                            <input type="hidden" name="supprExpPro" value="<?= $expPro->getIdexperience_pro() ?>">
+                            <button type="submit" class="btn btn-danger fas fa-trash-alt" ></button>
+                        </form>
+                    </td>
+                    <td>
+                        <button type="button" class="btn btn-primary fas fa-edit" data-toggle="modal" data-target="#updateExpPro<?=$expPro->getIdexperience_pro() ?>"></button>
+                    </td>
+                    <td>
+                        <button type="button" class="btn btn-primary fas fa-search" data-toggle="modal" data-target="#detailExpPro<?=$expPro->getIdexperience_pro() ?>"></button>
+                    </td>
+                </tr>
+                <!--  formulaire mise à d'une experience professionel -->
+                <div class="modal" id="updateExpPro<?=$expPro->getIdexperience_pro() ?>">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <form id="updateExpPro" method="post" action="/updateExpPro">
+
+                                <!-- Modal Header -->
+                                <div class="modal-header">
+                                    <h4 class="modal-title">mise à jour</h4>
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                </div>
+
+                                <!-- Modal body formulaire mise à jour Diplome -->
+                                <div class="modal-body">
+                                    <div class="form-group row d-none">
+                                        <label for="idExpProUpdate" class="col-sm-4 col-form-label">Id</label>
+                                        <div class="col-sm-8">
+                                            <input type="number" class="form-control" name="idExpProUpdate" id="idExpProUpdate" value="<?=$expPro->getIdexperience_pro() ?>" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="date_entrerExpPro" class="col-sm-4 col-form-label">Date d'entrer</label>
+                                        <div class="col-sm-8">
+                                            <input type="date" class="form-control" name="date_entrerExpPro" id="date_entrerExpPro" value="<?=$expPro->getDate_entrer() ?>" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row" >
+                                        <label for="date_sortieExpPro" class="col-sm-4 col-form-label">Date de sortie</label>
+                                        <div class="col-sm-8">
+                                            <input type="date" class="form-control" name="date_sortieExpPro" id="date_sortieExpPro" value="<?=$expPro->getDate_sortie() ?>" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group row">
+                                        <label for="nom_boiteExpPro" class="col-sm-4 col-form-label">Nom de la boite</label>
+                                        <div class="col-sm-8">
+                                            <input type="text" class="form-control" name="nom_boiteExpPro" id="nom_boiteExpPro" value="<?=$expPro->getNom_boite() ?>" required>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="descriptionExpPro">description:</label>
+                                        <textarea class="form-control" rows="5" name="descriptionExpPro" id="descriptionExpPro" required ><?=$expPro->getDescription(); ?></textarea>
+                                    </div>
+                                </div>
+
+                                <!-- Modal footer -->
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-primary float-right">Mettre à jour</button>
+                                    <button type="button" class="btn btn-danger" data-dismiss="modal">Fermer</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- modal d'affichage du détail d'une exp pro-->
+                <div class="modal" id="detailExpPro<?=$expPro->getIdexperience_pro() ?>">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <!-- Modal Header -->
+                            <div class="modal-header">
+                                <h4 class="modal-title">Détail de l'expérience pro</h4>
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            </div>
+
+                            <!-- Modal body formulaire mise à jour Diplome -->
+                            <div class="modal-body">
+                                <div class="container-fluid">
+
+                                    <dl class="row">
+                                        <dt class="col-sm-6">Date d' entrer:</dt>
+                                        <dd class="col-sm-6"><?= $expPro->getDate_entrer() ?></dd>
+                                    </dl>
+                                    <dl class="row">
+                                        <dt class="col-sm-6">Date de sortie:</dt>
+                                        <dd class="col-sm-6"><?= $expPro->getDate_sortie() ?></dd>
+                                    </dl>
+                                    <dl class="row">
+                                        <dt class="col-sm-6">Nom de la boite:</dt>
+                                        <dd class="col-sm-6"><?= $expPro->getNom_boite() ?></dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>Description: </dt>
+                                        <dd style="word-wrap: break-word;">
+                                            <?= $expPro->getDescription() ?>
+                                        </dd>
+
+
+                                    </dl>
+                                </div>
+                            </div>
+                        </div>
+                        <?php endforeach; ?>
+                <tbody>
+            </table>
+        </div>
+    </div>
+    <!--  formulaire Creation Exp pro-->
+    <div class="modal" id="createExpPro">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form method="post" action="/createExpPro">
+                    <!-- Modal Header -->
+                    <div class="modal-header">
+                        <h4 class="modal-title">Nouvelle expérience professionnel</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+
+                    <!-- Modal body -->
+                    <div class="modal-body">
+                        <div class="form-group row">
+                            <label for="date_entrerExpProCreate" class="col-sm-4 col-form-label">Date d'entrer</label>
+                            <div class="col-sm-8">
+                                <input type="date" class="form-control" name="date_entrerExpProCreate" id="date_entrerExpProCreate" value="<?$diplome ?>" required>
+                            </div>
+                        </div>
+                        <div class="form-group row" >
+                            <label for="date_sortieExpProCreate" class="col-sm-4 col-form-label">Date de sortie</label>
+                            <div class="col-sm-8">
+                                <input type="date" class="form-control" name="date_sortieExpProCreate" id="date_sortieExpProCreate" value="" required>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="nom_boiteExpProCreate" class="col-sm-4 col-form-label">Nom de boite</label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" name="nom_boiteExpProCreate" id="nom_boiteExpProCreate" value="" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="descriptionExpProCreate">description:</label>
+                            <textarea class="form-control" rows="5" name="descriptionExpProCreate" id="descriptionExpProCreate" required ></textarea>
                         </div>
                     </div>
 
